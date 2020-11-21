5510893cb4e8f34f0777cc381d33e444b73f441b
kos-vkg <kvertenov@mail.ru>
2020-11-21 20:28:09 +0300

    loaded files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int final_value = current_balance + transfer_amount;
+
+        System.out.println(final_value );
+    }
+}
